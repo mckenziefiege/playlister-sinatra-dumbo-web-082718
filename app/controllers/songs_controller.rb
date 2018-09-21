5f5401613b2b3551b484be4ac3c60d926bc8d207
jkhaha <jha2@wellesley.edu>
2018-09-21 10:35:40 -0400

    28 errors

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -5,13 +5,22 @@ class SongsController < ApplicationController
   end
 
   get '/songs/new' do
-    erb :new
+    @all_genres = Genre.all
+    @all_artists = Artist.all
+    erb :"songs/new"
   end
 
   post '/songs' do
-  end 
+
+    @song = Song.find_or_create_by(params[:song])
+    @artist = Artist.find_or_create_by(params[:artist])
+  binding.pry
+    #flash[:message] = "Successfully created song."
+    redirect to("/songs/#{@song.slug}")
+  end
 
   get '/songs/:slug' do
+    erb :"/songs/show"
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,14 @@
 # Add seed data here. Seed your database with `rake db:seed`
+Genre.destroy_all
+Song.destroy_all
+Artist.destroy_all
 
+pop = Genre.find_or_create_by(name: "pop")
+rock = Genre.find_or_create_by(name: "rock")
+rap = Genre.find_or_create_by(name: "rap")
+
+hello = Song.find_or_create_by(name: "hello")
+goodbye = Song.find_or_create_by(name: "goodbye")
+
+adele = Artist.find_or_create_by(name: "adele")
+drake = Artist.find_or_create_by(name: "drake")
